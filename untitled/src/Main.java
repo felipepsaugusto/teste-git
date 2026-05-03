dbf6af0730d79f28df5b1b980377cc1d3c1f365e
felipepsaugusto <felipepsaugusto@gmail.com>
2026-05-03 14:55:14 -0300

    Conexão simples com o database

---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+void main() {
+
+    String url = "jdbc:mysql://127.0.0.1:3306/course_jdbc";
+    String password = "";
+    String username = "root";
+    String query = "select * from users";
+
+    try {
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("user_name"));
+        }
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
