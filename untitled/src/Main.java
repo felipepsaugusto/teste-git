e928bbfc4a9ee8393a223b6cfa21dc63160d8641
felipepsaugusto <felipepsaugusto@gmail.com>
2026-05-03 15:25:03 -0300

    Atualizado String query

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,12 +5,11 @@ void main() {
     String url = "jdbc:mysql://127.0.0.1:3306/course_jdbc";
     String password = "";
     String username = "root";
-    String query = "select * from users";
 
     try {
         Connection connection = DriverManager.getConnection(url, username, password);
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = statement.executeQuery("select * from users");
         while(resultSet.next()){
             System.out.println(resultSet.getString("user_name") + " : " + resultSet.getString("password"));
         }
