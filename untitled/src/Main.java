cbec266d52e1773a126a5eeca539db4e56de6ec2
felipepsaugusto <felipepsaugusto@gmail.com>
2026-05-04 22:04:09 -0300

    Atualizações SQL

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,14 +6,37 @@ void main() {
     String password = "";
     String username = "root";
 
-    try {
+    try{
         Connection connection = DriverManager.getConnection(url, username, password);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from users");
-        while(resultSet.next()){
-            System.out.println(resultSet.getString("user_name") + " : " + resultSet.getString("password"));
+        PreparedStatement preparedStatement = connection.prepareStatement("Insert into users" + " (user_name, password, email)" + " values " +
+                "(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, "Dante");
+        preparedStatement.setString(2, "pequenininhasdds");
+        preparedStatement.setString(3, "vergilol@yahoo.com");
+        int value = preparedStatement.executeUpdate();
+        if(value > 0){
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while(resultSet.next()){
+                int id = resultSet.getInt(1);
+                System.out.println(id);
+            }
+        }
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+
+    /*try{
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement st = connection.createStatement();
+        ResultSet tableUsers = st.executeQuery("SELECT * FROM users");
+        System.out.println("------Users------");
+        while(tableUsers.next()){
+            System.out.println(tableUsers.getString("user_name") + " : " + tableUsers.getString("password")
+            + " : " + tableUsers.getString("email"));
         }
     } catch (SQLException e) {
         throw new RuntimeException(e);
     }
+    */
 }
