f33199cf1dc6c71d384c4f66919c6d7cd99a927f
felipepsaugusto <felipepsaugusto@gmail.com>
2026-05-04 22:31:29 -0300

    Atualizar tupla SQL

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -8,35 +8,18 @@ void main() {
 
     try{
         Connection connection = DriverManager.getConnection(url, username, password);
-        PreparedStatement preparedStatement = connection.prepareStatement("Insert into users" + " (user_name, password, email)" + " values " +
-                "(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, "Dante");
-        preparedStatement.setString(2, "pequenininhasdds");
-        preparedStatement.setString(3, "vergilol@yahoo.com");
-        int value = preparedStatement.executeUpdate();
-        if(value > 0){
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            while(resultSet.next()){
-                int id = resultSet.getInt(1);
-                System.out.println(id);
-            }
-        }
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE users" + " set user_name = ? " + " where " + "(idusers = 1)"
+        );
+
+        preparedStatement.setString(1, "Vergil");
+        int alt = preparedStatement.executeUpdate();
+        System.out.println(alt);
+
+
 
     } catch (SQLException e) {
         throw new RuntimeException(e);
     }
 
-    /*try{
-        Connection connection = DriverManager.getConnection(url, username, password);
-        Statement st = connection.createStatement();
-        ResultSet tableUsers = st.executeQuery("SELECT * FROM users");
-        System.out.println("------Users------");
-        while(tableUsers.next()){
-            System.out.println(tableUsers.getString("user_name") + " : " + tableUsers.getString("password")
-            + " : " + tableUsers.getString("email"));
-        }
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
-    }
-    */
 }
